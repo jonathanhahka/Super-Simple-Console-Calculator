906898420943a60b61d411f3a1eeb74eae4f9c1b
Jonathan Hahka <66832877+jonathanhahka@users.noreply.github.com>
2021-01-03 14:08:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oper8;
+            bool valid = true;
+            bool success = true;
+
+
+            // Asks for user input of an operator until correct.
+
+            do
+            {
+                Console.Write("Enter an operator (+,-,*, or /): ");
+                oper8 = Console.ReadLine();
+                if (oper8 == "+")
+                    valid = true;
+                else if (oper8 == "-")
+                    valid = true;
+                else if (oper8 == "*")
+                    valid = true;
+                else if (oper8 == "/")
+                    valid = true;
+                else
+                    valid = false;
+
+            }
+            while (!valid);
+
+            // If operator was valid, writes message.
+
+
+                if (oper8 == "+")
+                    
+                {
+                    Console.WriteLine("Nice, you entered +.");
+                    valid = true;
+                }
+                else if (oper8 == "-")
+                {
+                    Console.WriteLine("Nice, you entered -.");
+                    valid = true;
+                }
+
+                else if (oper8 == "*")
+                {
+                    Console.WriteLine("Nice, you entered *.");
+                    valid = true;
+                }
+                else if (oper8 == "/")
+                {
+                    Console.WriteLine("Nice, you entered /.");
+                        valid = true;
+                
+                }
+                if (oper8 == "+")
+
+                    console.writeline(num1 + num2);
+                else if
+                    (oper8 == "-")
+                    console.writeline(num1 - num2);
+                else if
+                    (oper8 == "*")
+                    console.writeline(num1 * num2);
+                else if
+                    (oper8 == "/")
+                    console.writeline(num1 / num2);
+                else
+                    console.writeline("you screwed up.");
+                }
+            
+                Console.Write("Enter a number: ");
+                string number1 = (Console.ReadLine());
+                double num1;
+                if (double.TryParse(number1, out num1))
+                {
+                    Console.WriteLine(num1);
+                }
+
+
+                Console.Write("Enter a second number: ");
+                string number2 = (Console.ReadLine());
+                double num2;
+                if (double.TryParse(number2, out num2))
+                {
+                    Console.WriteLine("The answer is: " + num1 + num2);
+                }
+
+                Console.WriteLine("The answer to " + (num1 + oper8 + num2 + " is:"));
+            }
+
+
+            
+            }
+
+
+
+            {
+                Console.ReadKey();
+            }
+
+
+
+            
+        }
+    }
+
+}
---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+            string oper8;
+
+
+            {
+                Console.Write("Enter an operator: ");
+                oper8 = Console.ReadLine();
+            }
+            {
+                if (oper8 == "+")
+                {
+                    Console.WriteLine("Nice, you know what an operator is, genius!");
+                }
+                else if (oper8 == "-")
+                {
+                    Console.WriteLine("Nice, you know what an operator is, genius!");
+                }
+                else if (oper8 == "*")
+                {
+                    Console.WriteLine("Nice, you know what an operator is, genius!");
+                }
+                else if (oper8 == "/")
+                {
+                    Console.WriteLine("Nice, you know what an operator is, genius!");
+                }
+
+                //while (!oper8.Equals(string.Empty));            
+
+
+
+                {
+
+                    Console.Write("Enter a number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+
+                    {
+                        Console.Write("Enter a second number: ");
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                    }
+                    {
+                        Console.WriteLine("The answer to " + (num1 + " " + oper8 + " " + num2 + " is: "));
+
+                    }
+                    {
+                        if (oper8 == "+")
+
+                            Console.WriteLine(num1 + num2);
+                        else if
+                            (oper8 == "-")
+                            Console.WriteLine(num1 - num2);
+                        else if
+                            (oper8 == "*")
+                            Console.WriteLine(num1 * num2);
+                        else if
+                            (oper8 == "/")
+                            Console.WriteLine(num1 / num2);
+                        else
+                            Console.WriteLine("You screwed up.");
+                    }
+
+                    
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
